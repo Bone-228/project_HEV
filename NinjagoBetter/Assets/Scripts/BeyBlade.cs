6297d35f08b4adf220b49ae7b9734795a415d024
MarekMS <marekstupka000@gmail.com>
2026-02-16 08:44:22 +0100

    feat(Scripts): add BeyBladeBase with health and movement logic (events)

--- NinjagoBetter/Assets/Scripts/BeyBlade.cs
+++ NinjagoBetter/Assets/Scripts/BeyBlade.cs
@@ -1,33 +1,47 @@
+using System;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public abstract class BeyBlade : MonoBehaviour
 {
-    public float moveSpeed = 5.0f;
-    public float spinSpeed = 500f;
-    public float weight = 1f;
-    public int maxHealth = 100;
+    [SerializeField]
+    protected float moveSpeed = 5.0f;
+    [SerializeField]
+    protected float spinSpeed = 500f;
+    [SerializeField]
+    protected float weight = 1f;
+    [SerializeField]
+    protected int maxHealth = 100;
 
-    protected int currentHealth;
+    public int CurrentHealth => currentHealth;
+    public bool IsAlive => currentHealth >= 0;
+
+    public event Action<BeyBlade> OnDeath;
+    public event Action<BeyBlade, int> OnHealthChanged;
+
+
     protected Rigidbody rb;
+    protected int currentHealth;
+    protected bool canMove = true;
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         currentHealth = maxHealth;
+        rb.maxAngularVelocity = 1000f;
     }
 
-    void Update()
-    {
-        Rotate();
-    }
 
     protected virtual void FixedUpdate()
     {
+        if (!canMove || !IsAlive) return;
+
         Vector3 input = GetInput();
         Move(input);
+        Rotate();
     }
+    protected abstract Vector3 GetInput();
 
     protected virtual void Move(Vector3 input)
     {
@@ -37,7 +51,6 @@ public abstract class BeyBlade : MonoBehaviour
         }
     }
 
-    protected abstract Vector3 GetInput();
 
     protected virtual void Rotate()
     {
@@ -46,7 +59,12 @@ public abstract class BeyBlade : MonoBehaviour
 
     public virtual void TakeDamage(int dmg)
     {
+        if (!IsAlive) return;
+
+
         currentHealth -= dmg;
+        OnHealthChanged?.Invoke(this, currentHealth);
+
         if (currentHealth <= 0)
         {
             Die();
@@ -55,6 +73,17 @@ public abstract class BeyBlade : MonoBehaviour
 
     protected virtual void Die()
     {
+        currentHealth = 0;
+        canMove = false;
+
+        OnDeath?.Invoke(this);
+
         gameObject.SetActive(false);
+    }
+
+    public void StopMovement()
+    {
+        canMove = false;
+        rb.angularVelocity = Vector3.zero;
     }
 }
