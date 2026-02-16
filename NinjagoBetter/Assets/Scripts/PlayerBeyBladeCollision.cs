7738cf63b5157821274f416bafaff3ac5a221cd1
MarekMS <marekstupka000@gmail.com>
2026-02-16 08:50:34 +0100

    feat(Scripts): Small details modified

--- NinjagoBetter/Assets/Scripts/PlayerBeyBladeCollision.cs
+++ NinjagoBetter/Assets/Scripts/PlayerBeyBladeCollision.cs
@@ -1,20 +1,30 @@
 using UnityEngine;
 
+
 public class PlayerBeyBladeCollision : MonoBehaviour
 {
-    public float knockback = 10f;
+    [SerializeField]
+    protected float knockback = 10f;
+    [SerializeField]
+    private int damage = 10;
+
+    private BeyBlade self;
+
+    private void Awake()
+    {
+        self = GetComponent<BeyBlade>();   
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
-        BeyBlade self = GetComponent<BeyBlade>();
         BeyBlade other = collision.collider.GetComponent<BeyBlade>();
 
         if (self == null || other == null) return;
 
         Vector3 dir = (other.transform.position - transform.position).normalized;
         Rigidbody rb = other.GetComponent<Rigidbody>();
-        rb.AddForce(dir * knockback * self.weight, ForceMode.Impulse);
+        rb.AddForce(dir * knockback * self.transform.localScale.magnitude, ForceMode.Impulse);
 
-        other.TakeDamage(10);
+        other.TakeDamage(damage);
     }
 }
